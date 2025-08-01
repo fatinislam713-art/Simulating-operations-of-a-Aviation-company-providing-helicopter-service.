b5267e6e6759bf13f3125750a92a9da5cc7f30ff
ashiq547 <114108018+ashiq547@users.noreply.github.com>
2025-08-01 23:02:15 +0600

    Add files via upload

---
+++ 2222122 air plan Customer Service Agent and Passengers/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.air_plan_customer_service_agent_and_passengers_2222122 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.air_plan_customer_service_agent_and_passengers_2222122 to javafx.fxml;
+    exports com.example.air_plan_customer_service_agent_and_passengers_2222122;
+}
