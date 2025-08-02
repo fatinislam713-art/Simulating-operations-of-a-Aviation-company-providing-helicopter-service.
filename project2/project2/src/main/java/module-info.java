b6030a89888fff7e87f8fd515ba2e12fe12eb109
fatinislam713-art <fatinislam713@gmail.com>
2025-08-02 20:53:43 +0600

    Delete project2/project2 directory

--- project2/project2/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module cse213.project2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens cse213.project2 to javafx.fxml;
-    exports cse213.project2;
-}
