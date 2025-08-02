69e755acb7bd23096d24525275e079b4c29f4ab7
fatinislam713-art <fatinislam713@gmail.com>
2025-08-02 20:54:01 +0600

    Delete simulationofaAviationcompanyforHelicopterService directory

--- simulationofaAviationcompanyforHelicopterService/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.simulationofaaviationcompanyforhelicopterservice {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.simulationofaaviationcompanyforhelicopterservice to javafx.fxml;
-    exports com.example.simulationofaaviationcompanyforhelicopterservice;
-}
