29982f72dfe93f51b34ec2a4ca2ed861e6e3fbac
fatinislam713-art <fatinislam713@gmail.com>
2025-08-16 13:20:58 +0600

    Delete SimulatingOperationOfaAviationCompany2310898 directory

--- SimulatingOperationOfaAviationCompany2310898/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.simulatingoperationofaaviationcompany2310898 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.simulatingoperationofaaviationcompany2310898 to javafx.fxml;
-    exports com.example.simulatingoperationofaaviationcompany2310898;
-}
