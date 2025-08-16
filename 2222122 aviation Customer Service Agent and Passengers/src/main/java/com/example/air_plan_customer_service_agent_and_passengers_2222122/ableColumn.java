5bc805c99b383f4a060786918e11c344b693456a
ashiq547 <114108018+ashiq547@users.noreply.github.com>
2025-08-16 12:36:16 +0600

    Delete 2222122 aviation Customer Service Agent and Passengers directory

--- 2222122 aviation Customer Service Agent and Passengers/src/main/java/com/example/air_plan_customer_service_agent_and_passengers_2222122/ableColumn.java
+++
@@ -1,4 +0,0 @@
-package com.example.air_plan_customer_service_agent_and_passengers_2222122;
-
-public class ableColumn {
-}
--- 2222122 aviation Customer Service Agent and Passengers/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.air_plan_customer_service_agent_and_passengers_2222122 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens com.example.air_plan_customer_service_agent_and_passengers_2222122 to javafx.fxml;
-    exports com.example.air_plan_customer_service_agent_and_passengers_2222122;
-}
